df458572ed509e8ebaa39dab8c4b83c9aeb87258
Jeris Manning <the.goose.moose@gmail.com>
2019-12-21 03:23:25 -0700

    some endpoints

---
+++ database/model.js
@@ -0,0 +1,39 @@
+const db = require('../knex/knex.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findByUnique,
+  findCol
+};
+
+function find(table) {
+  return db(table).select('*');
+}
+
+// Accepts array of 2 strings [table, column]
+function findCol(tcArray) {
+
+  return db(tcArray[0]).select(tcArray[1]);
+}
+
+function findBy(filter) {
+
+  return db('parents').where(filter);
+}
+
+async function add(parent) {
+  const [id] = await db('parents').insert(parent);
+
+  return find('parents')
+    .where({ id })
+    .first();
+}
+
+function findByUnique(uniqueIdent) {
+  return db(uniqueIdent[0])
+    .select(uniqueIdent[1])
+    .where(uniqueIdent[2])
+    .first();
+}
---
+++ knex/migrations/20191219211024_giga1.js
@@ -0,0 +1,51 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('parents', (tbl) => {
+      tbl.increments('id').primary();
+      tbl
+        .string('username')
+        .unique()
+        .notNullable();
+      tbl.string('password').notNullable();
+    })
+    .createTable('children', (tbl) => {
+      tbl.increments('id').primary();
+      tbl.string('name').notNullable();
+      tbl
+        .integer('parent_id')
+        .references('id')
+        .inTable('parents')
+        .notNullable()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl
+        .integer('parent_2_id')
+        .references('id')
+        .inTable('parents')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('food_entries', (tbl) => {
+      tbl.increments('id').primary();
+      tbl.date('date');
+      tbl.integer('dairy');
+      tbl.integer('fruits');
+      tbl.integer('grains');
+      tbl.integer('proteins');
+      tbl.integer('vegetables');
+      tbl.integer('treats');
+      tbl
+        .integer('child_id')
+        .references('id')
+        .inTable('children');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('food_entries')
+    .dropTableIfExists('children')
+    .dropTableIfExists('parents');
+};
+
+//Consider using update to have foods eaten .json object
