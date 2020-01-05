1a660cfde807c8050d91fd99bf0e764ba92431d2
Jeris Manning <the.goose.moose@gmail.com>
2020-01-05 16:09:39 -0700

    _

--- api/server.js
+++ api/server.js
@@ -6,10 +6,10 @@ const config = require('../db/dbConfig.js');
 const authenticate = require('../auth/auth-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const parentRouter = require('../parent/parent-router');
-// const environment = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
-// const configuration = require('../knexfile')[environment]; // require environment's settings from knexfile
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('../knexfile')[environment]; 
 // const db = require('knex')(configuration); // connect to DB via knex using env's settings
-server.use(config());
+server.use(config);
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
