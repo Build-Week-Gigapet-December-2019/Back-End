ee85daae0a58a6b66bfa27243176933506262ca8
Jeris Manning <the.goose.moose@gmail.com>
2020-01-05 16:02:32 -0700

    environment tweaking

--- api/server.js
+++ api/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
+const config = require('../db/dbConfig.js');
 const authenticate = require('../auth/auth-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const parentRouter = require('../parent/parent-router');
-const environment = require('../db/dbConfig.js');
-const environment = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
-const configuration = require('../knexfile')[environment]; // require environment's settings from knexfile
-const db = require('knex')(configuration); // connect to DB via knex using env's settings
+// const environment = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
+// const configuration = require('../knexfile')[environment]; // require environment's settings from knexfile
+// const db = require('knex')(configuration); // connect to DB via knex using env's settings
+server.use(config());
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
